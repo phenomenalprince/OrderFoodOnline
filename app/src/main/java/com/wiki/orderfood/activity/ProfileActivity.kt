222573ff627141475277aa17ee6694baf8d0b6c4
Prince Sehrawat <wikisehrawat@gmail.com>
2020-08-24 11:20:08 +0530

    Initial commit

---
+++ app/src/main/java/com/wiki/orderfood/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.wiki.orderfood.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wiki.orderfood.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ app/src/main/java/com/wiki/orderfood/model/CartItems.kt
@@ -0,0 +1,8 @@
+package com.wiki.orderfood.model
+
+data class CartItems (
+    var itemId:String,
+    var itemName:String,
+    var itemPrice:String,
+    var restaurantId:String
+)
